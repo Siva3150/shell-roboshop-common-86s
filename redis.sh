395b6145e439f909b7c053870d48f3df61951122
Siva prasad <sivaprasad3150@gmail.com>
2026-02-08 18:36:14 +0530

    shell-script

--- redis.sh
+++ redis.sh
@@ -1,35 +1,10 @@
 #!/bin/bash 
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-USERID=$(id -u)
+source ./common.sh 
 
+app_name=redis 
 
-LOGS_FOLDER="/var/log/shell-script-logs"
-SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" #/var/log/shell-script-logs/redis-sh.log
-START_TIME=$(date +%s)
-
-
-mkdir -p $LOGS_FOLDER
-echo "Script started executed at: $(date)" | tee -a $LOG_FILE
-
-if [ $USERID -ne 0 ]; then 
-   echo "Error: Please run this script with root previlages" 
-   exit 1 # failure is other than 0
-fi 
-
-VALIDATE(){ # functions receive inputs through args just like shell script args
-    if [ $1 -ne 0 ]; then 
-      echo -e "$2... $R failed $N " | tee -a $LOG_FILE
-      exit 1 # failure is other than 0
-    else 
-     echo -e "$2...$G success $N" | tee -a $LOG_FILE
-    fi 
-
-}
+check_root
 
 ### Redis Installation ###
 dnf module disable redis -y &>>$LOG_FILE
@@ -47,6 +22,4 @@ VALIDATE $? "Enabling Redis"
 systemctl start redis &>>$LOG_FILE
 VALIDATE $? "Starting Redis"
 
-END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $START_TIME ))
-echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
+print_total_time
