f0fcc20c34286c6d2f1f3142b7c36b4abd8a807b
Siva prasad <sivaprasad3150@gmail.com>
2026-02-08 18:28:13 +0530

    shell-script

--- catalogue.sh
+++ catalogue.sh
@@ -1,86 +1,15 @@
 #!/bin/bash 
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-USERID=$(id -u)
+source ./cpmmon.sh 
+
+app_name=catalogue 
+
+check_root
+app_setup
+nodejs_setup
+systemd_setup
 
 
-LOGS_FOLDER="/var/log/shell-script-logs"
-SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-SCRIPT_DIR=$PWD
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" #/var/log/shell-script-logs/18-logs.log
-MONGODB_HOST=mongodb.sivadevops.space
-START_TIME=$(date +%s)
-
-
-mkdir -p $LOGS_FOLDER
-echo "Script started executed at: $(date)" | tee -a $LOG_FILE
-
-if [ $USERID -ne 0 ]; then 
-   echo "Error: Please run this script with root previlages" 
-   exit 1 # failure is other than 0
-fi 
-
-VALIDATE(){ # functions receive inputs through args just like shell script args
-    if [ $1 -ne 0 ]; then 
-      echo -e "$2... $R failed $N " | tee -a $LOG_FILE
-      exit 1 # failure is other than 0
-    else 
-     echo -e "$2...$G success $N" | tee -a $LOG_FILE
-    fi 
-
-}
-
-##### NodeJS ####
-dnf module disable nodejs -y &>>$LOG_FILE
-VALIDATE $? "Disabling default nodejs"
-
-dnf module enable nodejs:20 -y &>>$LOG_FILE
-VALIDATE $? "Enabling nodejs 20"
-
-dnf install nodejs -y &>>$LOG_FILE
-VALIDATE $? "Installing nodejs"
-
-### System user ###
-id roboshop &>>$LOG_FILE
-if [ $? -ne 0 ]; then 
- useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
- VALIDATE $? "Creating roboshop user"
-else 
-  echo -e "User already exist ... $Y SKIPPING $N"
-fi
-
-mkdir -p /app 
-VALIDATE $? "Creating /app directory"
-
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip &>>$LOG_FILE
-VALIDATE $? "Downloading code"
-
-cd /app 
-VALIDATE $? "Changing to /app directory"
-
-rm -rf /app/*
-VALIDATE $? "Removing existing code"
-
-unzip /tmp/catalogue.zip &>>$LOG_FILE
-VALIDATE $? "unzipping the code"
-
-npm install &>>$LOG_FILE
-VALIDATE $? "Installing dependencies"
-
-cp $SCRIPT_DIR/catalogue.service /etc/systemd/system/catalogue.service
-VALIDATE $? "Copying catalogue service"
-
-systemctl daemon-reload &>>$LOG_FILE
-VALIDATE $? "daemon reload"
-
-systemctl enable catalogue  &>>$LOG_FILE
-VALIDATE $? "Enabling catalogue"
-
-systemctl start catalogue &>>$LOG_FILE
-VALIDATE $? "Starting catalogue"
 
 cp $SCRIPT_DIR/mongodb.repo /etc/yum.repos.d/mongo.repo
 VALIDATE $? "Copying mongo repo"
@@ -102,6 +31,9 @@ VALIDATE $? "Restarting catalogue"
 END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
+
+app_restart
+print_total_time
 
 
 
