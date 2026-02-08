17fd1db77074d7abba5f9bb067bdae7cfc87c547
Siva prasad <sivaprasad3150@gmail.com>
2026-02-08 18:47:40 +0530

    shell-script

--- user.sh
+++ user.sh
@@ -1,89 +1,14 @@
 #!/bin/bash 
 
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
-USERID=$(id -u)
+source ./common.sh 
+app_name=user 
 
+check_root
+app_setup
+nodejs_setup
+systemd_setup
+app_restart
+print_total_time
 
-LOGS_FOLDER="/var/log/shell-script-logs"
-SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
-SCRIPT_DIR=$PWD
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" #/var/log/shell-script-logs/18-logs.log
-MONGODB_HOST=mongodb.sivadevops.space
-START_TIME=$(date +%s)
-
-
-mkdir -p $LOGS_FOLDER
-echo "Script started executed at: $(date)" | tee -a $LOG_FILE
-
-if [ $USERID -ne 0 ]; then 
-   echo "Error: Please run this script with root previlages" 
-   exit 1 # failure is other than 0
-fi 
-
-VALIDATE(){ # functions receive inputs through args just like shell script args
-    if [ $1 -ne 0 ]; then 
-      echo -e "$2... $R failed $N " | tee -a $LOG_FILE
-      exit 1 # failure is other than 0
-    else 
-     echo -e "$2...$G success $N" | tee -a $LOG_FILE
-    fi 
-
-}
-
-##### NodeJS ####
-dnf module disable nodejs -y &>>$LOG_FILE
-VALIDATE $? "Disabling default nodejs"
-
-dnf module enable nodejs:20 -y &>>$LOG_FILE
-VALIDATE $? "Enabling nodejs 20"
-
-dnf install nodejs -y &>>$LOG_FILE
-VALIDATE $? "Installing nodejs"
-
-### System user ###
-id roboshop &>>$LOG_FILE
-if [ $? -ne 0 ]; then 
- useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
- VALIDATE $? "Creating roboshop user"
-else 
-  echo -e "User already exist ... $Y SKIPPING $N"
-fi
-
-mkdir -p /app 
-VALIDATE $? "Creating /app directory"
-
-curl -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip &>>$LOG_FILE
-VALIDATE $? "Downloading code"
-
-cd /app 
-VALIDATE $? "Changing to /app directory"
-
-rm -rf /app/*
-VALIDATE $? "Removing existing code"
-
-unzip /tmp/user.zip &>>$LOG_FILE
-VALIDATE $? "unzipping the code"
-
-npm install &>>$LOG_FILE
-VALIDATE $? "Installing dependencies"
-
-cp $SCRIPT_DIR/user.service /etc/systemd/system/user.service &>>$LOG_FILE
-VALIDATE $? "Copying user service"
-
-systemctl daemon-reload &>>$LOG_FILE
-VALIDATE $? "daemon reload"
-
-systemctl enable user  &>>$LOG_FILE
-VALIDATE $? "Enabling user"
-
-systemctl start user &>>$LOG_FILE
-VALIDATE $? "Starting user"
-
-END_TIME=$(date +%s)
-TOTAL_TIME=$(( $END_TIME - $START_TIME ))
-echo -e "Script executed in: $Y $TOTAL_TIME Seconds $N"
 
 
